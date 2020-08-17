13a8c7822625fad2fb9e783335166605a13d53ae
Maxim Zaytsev <maxim.zaytsev.1305@gmail.com>
2020-08-17 11:52:00 +0300

    Add save score, Add icon, Update project

--- app/src/main/java/com/merive/press1mtimes/MainActivity.java
+++ app/src/main/java/com/merive/press1mtimes/MainActivity.java
@@ -1,25 +1,39 @@
 package com.merive.press1mtimes;
 
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView scoreTV;
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         scoreTV = findViewById(R.id.score);
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this.getBaseContext());
+        String score = sharedPreferences.getString("score", "");
+        while (score.length() != 6) {
+            score = "0" + score;
+        }
+        scoreTV.setText(score);
     }
 
     public void buttonClick(View view) {
         int score = Integer.parseInt(String.valueOf(scoreTV.getText()));
         if (score == 999999) {
+            Toast.makeText(MainActivity.this, "You press button 1M times...",
+                    Toast.LENGTH_LONG).show();
             scoreTV.setText("000000");
         } else {
             score += 1;
@@ -27,7 +41,9 @@ public class MainActivity extends AppCompatActivity {
             while (result.length() != 6) {
                 result = "0" + result;
             }
+            sharedPreferences.edit().putString("score", result).apply();
             scoreTV.setText(result);
         }
     }
+
 }
