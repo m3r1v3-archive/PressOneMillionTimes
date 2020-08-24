4382c0ea9bcaa0b3aa1f66c73bdd3cd6e957fe02
Maxim Zaytsev <maxim.zaytsev.1305@gmail.com>
2020-08-24 21:44:01 +0300

    Add splash screen, Update README.md, Update AndroidManifest.xml

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Press1MTimes
-Mini Android Game
-
+### _Android Clicker-Game_
 A simple game in which the main task is to find out what happens after a million clicks on the red button...
+____
--- app/src/main/java/com/merive/press1mtimes/MainActivity.java
+++ app/src/main/java/com/merive/press1mtimes/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // Init Activity
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
