f26ce193410e7525a440c500941deb303147ccd4
Maxim Zaytsev <maxim.zaytsev.1305@gmail.com>
2020-09-26 14:33:50 +0300

    Realise v1.2.3
    Add pascal egg,
    Update About.java,
    Update strings.xml

--- app/src/main/java/com/merive/press1mtimes/About.java
+++ app/src/main/java/com/merive/press1mtimes/About.java
@@ -2,11 +2,19 @@ package com.merive.press1mtimes;
 
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class About extends AppCompatActivity {
+
+    Button exit;
+    ImageView pascal;
+    Handler handler;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,10 +22,22 @@ public class About extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        /* Init variables */
+        exit = findViewById(R.id.exit);
+        pascal = findViewById(R.id.pascal);
     }
 
     public void exitClick(View view) {
-        /* Finish Activity */
-        finish();
+        /* Set visibility exit & pascal */
+        exit.setVisibility(View.INVISIBLE);
+        pascal.setVisibility(View.VISIBLE);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                finish();
+            }
+        }, 10);
     }
 }
